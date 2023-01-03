9d5edb9421a0be1e4b402f319c869afd125bee1b
Pedro Victor <pedro20002victor@gmail.com>
2023-01-03 10:23:51 -0300

    Scalar types | Types Querys | Resolvers and functions

--- server.js
+++ server.js
@@ -2,25 +2,44 @@ const { gql, ApolloServer } = require('apollo-server');
 
 // Create a Query
 const typeDefs = gql`
+  # Ponto de entrada para pesquisas 
   type Query {
-    hello: String
+    age: Int
+    cash: Float
+    name: String
+    active: Boolean
+    id: ID
   }
 `;
 
 // Create resolver query
 const resolvers = {
   Query: {
-    hello: () => {
-      return 'Hello world!'
+    age: () => {
+      return 21;
+    },
+    cash: () => {
+      return 7.800;
+    },
+    name: () => {
+      return 'Pedro Victor';
+    },
+    active: () => {
+      return true;
+    },
+    id: () => {
+      return 9;
     }
   }
-};
+}
+
+console.log(resolvers);
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs, 
   resolvers
-})
+});
 
 server.listen().then(({ url }) => {
   console.log(`Server running at ${url}`);
-});
+})
