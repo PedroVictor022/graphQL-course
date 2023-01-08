934dfb691edf3aac792f1312751d872b9429f0d5
Pedro Victor <pedro20002victor@gmail.com>
2023-01-08 19:41:16 -0300

    First arg - resolver

--- server.js
+++ server.js
@@ -3,63 +3,42 @@ const { gql, ApolloServer } = require('apollo-server');
 
 const users = [
   {
-    id: '1',
-    name: 'Pedro Victor',
-    age: '20'
+    id: 1,
+    name: 'Dexter',
+    email: 'dexter1@gmail.com',
+    phone_fix: '910201120912'
   },
   {
-    id: '2',
-    name: 'Luiza',
-    age: '20'
+    id: 1,
+    name: 'Dexter',
+    email: 'dexter2@gmail.com',
+    phone_fix: '910201120913'
   }
 ]
 
-// Create a Query
 const typeDefs = gql`
-  # Ponto de entrada para pesquisas 
-  type Query {
-    # Props: Types 
-    age: Int
-    cash: Float
-    name: String
-    active: Boolean
+# Prop | Type
+  type User {
     id: ID,
-    techs: [String!]!
-    # users: [String!]! 
-    # [String]! o array nao pode estar vazio
+    name: String,
+    email: String,
+    phone: String
+  }
+  type Query {
+    user: User
   }
 `;
 // Create resolver query
 const resolvers = {
-  Query: {
-    // Cada query tem q ser denomida como funcao 
-    age: () => {
-      return 21;
-    },
-    cash: () => {
-      return 13.900;
-    },
-    name: () => {
-      return 'Pedro';
-    },
-    active: () => {
-      return true;
-    },
-    id: () => {
-      return 01;
-    },
-    // users: () => {
-    //   return users
-    // },
-    techs: () => {
-      return [
-        'GraphQL',
-        'Javascript',
-        'Python',
-        1, 
-        2,
-      ]
+  User: {
+    phone: (obj) => {
+      return obj.phone_fix
     }
+  },
+  Query: {
+   user: () => {
+    return users[0];
+   }
   }
 }
 
